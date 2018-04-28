88ab00c5b3f5d5015f018fed6b64b6cb96f4dbe4
Paul Barthmaier <paul@barthmaier.rocks>
2018-04-28 01:44:07 -0400

    spacing

--- includes/privacy.php
+++ includes/privacy.php
@@ -1,17 +1,18 @@
 <?php
 /**
  * Code to aid with user data privacy, e.g. GDPR compliance
- * 
+ *
  * @since  1.9.5
  */
 
-/** 
+/**
  * Add suggested Privacy Policy language for PMPro
+ *
  * @since 1.9.5
  */
-function pmpro_add_privacy_policy_content() {	
+function pmpro_add_privacy_policy_content() {
 	// Check for support.
-	if ( ! function_exists( 'wp_add_privacy_policy_content') ) {
+	if ( ! function_exists( 'wp_add_privacy_policy_content' ) ) {
 		return;
 	}
 
@@ -23,13 +24,14 @@ add_action( 'admin_init', 'pmpro_add_privacy_policy_content' );
 
 /**
  * Register the personal data eraser for PMPro
+ *
  * @param array $erasers All erasers added so far
  */
 function pmpro_register_personal_data_erasers( $erasers ) {
 	$erasers[] = array(
- 		'eraser_friendly_name' => __( 'Paid Memberships Pro Data' ),
- 		'callback'             => 'pmpro_personal_data_eraser',
- 	);
+		'eraser_friendly_name' => __( 'Paid Memberships Pro Data' ),
+		'callback'             => 'pmpro_personal_data_eraser',
+	);
 
 	return $erasers;
 }
@@ -37,27 +39,27 @@ add_filter( 'wp_privacy_personal_data_erasers', 'pmpro_register_personal_data_er
 
 /**
  * Personal data eraser for PMPro data.
+ *
  * @param string $email_address Email address of the user to be erased.
  * @param int    $page          For batching
  */
 function pmpro_personal_data_eraser( $email_address, $page = 1 ) {
 	// Erase any data we have about this user.
-	
 	// Keep track of how many items are removed and remaining.
-	
 	// Set done to false if we still have stuff to erase.
 	$done = true;
 
 	return array(
- 		'num_items_removed'  => 0,
- 		'num_items_retained' => 0,
- 		'messages'           => array(),
- 		'done'               => $done,
- 	);
+		'num_items_removed'  => 0,
+		'num_items_retained' => 0,
+		'messages'           => array(),
+		'done'               => $done,
+	);
 }
 
 /**
  * Register the personal data exporter for PMPro.
+ *
  * @param array $exporters All exporters added so far
  */
 function pmpro_register_personal_data_exporters( $exporters ) {
@@ -81,19 +83,19 @@ function pmpro_personal_data_exporter( $email_address, $page = 1 ) {
 	// What user is this?
 	$user = get_user_by( 'email', $email_address );
 
-	if( !empty( $user ) ) {
+	if ( ! empty( $user ) ) {
 		// Add data stored in user meta.
 		$personal_user_meta_fields = pmpro_get_personal_user_meta_fields();
 		$sqlQuery = "SELECT meta_key, meta_value
 					 FROM $wpdb->usermeta
-					 WHERE user_id = '" . intval($user->ID) . "'
+					 WHERE user_id = '" . intval( $user->ID ) . "'
 					 	AND meta_key IN('" . implode( "', '", array_keys( $personal_user_meta_fields ) ) . "')";
 		$personal_user_meta_data = $wpdb->get_results( $sqlQuery, OBJECT_K );
-		
+
 		$user_meta_data_to_export = array();
-		foreach( $personal_user_meta_fields as $key => $name ) {
-			if( !empty( $personal_user_meta_data[$key] ) ) {
-				$value = $personal_user_meta_data[$key]->meta_value;
+		foreach ( $personal_user_meta_fields as $key => $name ) {
+			if ( ! empty( $personal_user_meta_data[ $key ] ) ) {
+				$value = $personal_user_meta_data[ $key ]->meta_value;
 			} else {
 				$value = '';
 			}
@@ -110,13 +112,12 @@ function pmpro_personal_data_exporter( $email_address, $page = 1 ) {
 			'item_id'     => "user-{$user->ID}",
 			'data'        => $user_meta_data_to_export,
 		);
-		
 
 		// Add membership history.
-		$sqlQuery = "SELECT * FROM $wpdb->pmpro_memberships_users WHERE user_id = '" . intval($user->ID) . "' ORDER BY id DESC";
+		$sqlQuery = "SELECT * FROM $wpdb->pmpro_memberships_users WHERE user_id = '" . intval( $user->ID ) . "' ORDER BY id DESC";
 		$history = $wpdb->get_results( $sqlQuery );
-		foreach( $history as $item ) {
-			if( $item->enddate === null || $item->enddate == '0000-00-00 00:00:00' ) {
+		foreach ( $history as $item ) {
+			if ( $item->enddate === null || $item->enddate == '0000-00-00 00:00:00' ) {
 				$item->enddate = __( 'Never', 'paid-memberships-pro' );
 			} else {
 				$item->enddate = date( get_option( 'date_format' ), strtotime( $item->enddate, current_time( 'timestamp' ) ) );
@@ -125,7 +126,7 @@ function pmpro_personal_data_exporter( $email_address, $page = 1 ) {
 			$history_data_to_export = array(
 				array(
 					'name'  => __( 'Level ID', 'paid-memberships-pro' ),
-					'value' => $item->membership_id, 
+					'value' => $item->membership_id,
 				),
 				array(
 					'name'  => __( 'Start Date', 'paid-memberships-pro' ),
@@ -159,7 +160,6 @@ function pmpro_personal_data_exporter( $email_address, $page = 1 ) {
 
 		// Add order history.
 		// TODO: grab order history and put into $data_to_export
-		
 	}
 
 	$done = true;
